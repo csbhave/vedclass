d49d4b688e80df0f171f52cd908aeee28669de02
chinmay <csbhavegithub@gmail.com>
2020-02-05 14:28:52 +0530

    add skeleton of vedclass project

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>VED Educational Academy</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<style>
+    .container-fluid{
+        margin-top:30px;
+        border:1px solid black;
+    }
+</style>
+</head>
+<body>
+<div class="container-fluid" style="height:70px;">
+<div class="row"">
+    <div class="col-12 col-lg-3 col-md-4 col-sm-12"style="border:1px solid black;">
+    <h1>Logo</h1>
+    </div>
+    <div class="col-9 col-lg-9 col-md-8 col-sm-12" style="border:1px solid black;">
+    <h1>Menubar</h1>
+    </div>
+</div>
+</div>
+<div class="container-fluid" style="height:490px;">
+    <h1>image-slider</h1>
+</div>
+
+<div class="container-fluid" style="height:600px;">
+    <h1>class portfolio</h1>
+</div>
+
+<div class="container-fluid" style="height:600px;">
+    <h1>our services</h1>
+</div>
+
+
+<div class="container-fluid" style="height:600px;">
+    <h1>Recent Achievement</h1>
+</div>
+
+<div class="container-fluid" style="height:600px;">
+    <h1>Student feedback</h1>
+</div>
+
+<div class="container-fluid" style="height:600px;">
+    <h1>Location</h1>
+</div>
+
+<div class="container-fluid" style="height:465px;">
+    <h1>contact us</h1>
+</div>
+
+
+<div class="container-fluid" style="height:200px;">
+    <h1>footer</h1>
+</div>
+
+
+</body>
+</html>
